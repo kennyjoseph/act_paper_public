e2f5c13fd362570751c9fd3b38dfc48a47ff61a7
Kenny Joseph <josephkena@gmail.com>
2015-10-21 07:35:10 -0400

    updating plotting code

--- R/gen_results.R
+++ R/gen_results.R
@@ -1,5 +1,7 @@
 library(reshape2)
 library(dplyr)
+library(Hmisc)
+library(ggplot2)
 
 full_res <- fread("R/results/full_final_out.tsv")
 setnames(full_res, c("fold","name","ntopic","random","p","alpsi","initsd","q","medrank","meanrank","top10","sumll","ndoc"))
@@ -18,13 +20,13 @@ full_res[name=='Full' & random==T]$name <- "No ACT"
 res <- full_res[,as.list(smean.cl.boot(-sumll/ndoc)), by=c("name","ntopic")]
 
 fig1 <- ggplot() 
-fig1 <- fig1 + geom_pointrange(data=res[ntopic >0], size=1.2, 
-                               aes(x=ntopic, ymin=Lower,ymax=Upper,y=Mean,color=name)) 
-fig1 <- fig1 + geom_line(data=res[ntopic >0],size=1.2, aes(x=ntopic,y=Mean,color=name)) 
-fig1 <- fig1 + geom_hline(data=res[ntopic==-1],size=1.2, aes(yintercept=Upper,color=name))
-fig1 <- fig1 + geom_hline(data=res[ntopic==-1], size=1.2,aes(yintercept=Lower,color=name))
+fig1 <- fig1 + geom_line(data=res[ntopic >0],size=1.2, aes(x=ntopic,y=Mean,linetype=name)) 
+fig1 <- fig1 + geom_hline(data=res[ntopic==-1],size=1.2, aes(yintercept=Upper,linetype=name))
+fig1 <- fig1 + geom_hline(data=res[ntopic==-1], size=1.2,aes(yintercept=Lower,linetype=name))
 fig1 <- fig1 + ylab("Average Perplexity") + xlab("Number of Topics")
-fig1 <- fig1 + scale_color_discrete(guide=guide_legend("Model",title.position='top',title.hjust=.5,label.position='bottom',label.hjust=.5)) 
+fig1 <- fig1 + geom_pointrange(data=res[ntopic >0], size=1.2, 
+                               aes(x=ntopic, ymin=Lower,ymax=Upper,y=Mean,linetype=name)) 
+fig1 <- fig1 + scale_linetype_discrete(guide=guide_legend("Model",title.position='top',title.hjust=.5,label.position='bottom',label.hjust=.5,keywidth=5)) 
 ggsave("paper/results_1.png",fig1,h=6,w=10,dpi=400)
 
 
@@ -41,7 +43,7 @@ setnames(bigram,"V11","Bigram_Rank")
 merged <- merge(full[,c("ind","V9","Full","Full_Rank"),with=F],
            bigram[,c("ind","V9","Bigram","Bigram_Rank"),with=F],by=c("ind","V9"))
 
-fig2 <- ggplot(merged, aes(Bigram,Full)) + geom_point(alpha=.3) + geom_abline(slope=1,color='red',size=1.2)
+fig2 <- ggplot(merged, aes(Bigram,Full)) + geom_point(alpha=.5) + geom_abline(slope=1,color='grey50',size=3)
 fig2 <- fig2 + xlab("Likelihood of Test Point - Bigram Model") + ylab("Likelihood of Test Point- Best Full Model")
 ggsave("paper/results_2.png",fig2,h=6,w=6,dpi=400)
 
@@ -55,10 +57,10 @@ for( i in c(1:length(cutoffs))){
   df[i,"Bigram"] <- -sum(log(dat$Bigram))/nrow(dat)
 }
 
-fig3 <- ggplot(melt(df,id.vars="cutoff"), aes(cutoff,value,color=variable)) + geom_line(size=1.2) + geom_point(size=5)  
+fig3 <- ggplot(melt(df,id.vars="cutoff"), aes(cutoff,value,linetype=variable)) + geom_line(size=1.2) + geom_point(size=5)  
 fig3 <- fig3 + ylab("Perplexity") + xlab("Cutoff for Model Certainty") 
 
-fig3 <- fig3 + theme(legend.position='bottom') + scale_color_discrete(guide=guide_legend("Model",title.position='top',title.hjust=.5,label.position='bottom',label.hjust=.5,keywidth=4)) 
+fig3 <- fig3 + theme(legend.position=c(.8,.8)) + scale_linetype_discrete(guide=guide_legend("Model",title.position='top',title.hjust=.5,label.position='bottom',label.hjust=.5,keywidth=4))
 ggsave("paper/results_3.png",fig3,h=6,w=6,dpi=400)
 
 
@@ -113,13 +115,13 @@ yscale <- scale_y_continuous(limits=c(-4.3,4.3),breaks=c(-4.3,-2.1,0,2.1, 4.3))
 fig4 <-  ggplot(train[datatype==1 & from_data==0], 
                 aes(entity, new_mu_0,ymin=low,ymax=high, group=factor(topic)))
 fig4 <- fig4 + geom_pointrange(position=position_dodge(width=.8)) 
-fig4 <- fig4 + xlab("Behaviors") + ylab("") + geom_hline(y=0,color='red')
+fig4 <- fig4 + xlab("Behaviors") + ylab("") + geom_hline(y=0,color='grey')
 fig4 <- fig4 + facet_grid(epa~.,labeller=epa_labeller) 
 fig4 <- fig4 + theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1),legend.position='bottom') + yscale
 ggsave("paper/results_4.png",width=13,h=7.5,dpi=400)
 
 all_identities <- fig4 %+% train[datatype==0 & from_data==0] + xlab("Identities")
-ggsave("paper/all_identities.png",width=15,h=7,dpi=400)
+ggsave("paper/all_identities.png",width=19,h=7,dpi=400)
 
 pol <- c("democrat","republican")
 rel <- c("christian","jews","muslims","islamists","extremist","sunni")
@@ -128,13 +130,9 @@ of_interest <- rel #c(pol,rel,count)
                     
 fig5_data <- train[datatype==0& entity %in% of_interest]
 
-fig5_data$type <- "U.S. Politics"
-fig5_data[entity %in% rel]$type <- "Religious"
-fig5_data[entity %in% count]$type <- "Nationality"
-
 fig5_data$entity <- factor(fig5_data$entity,levels=of_interest)
 
-fig5 <- fig4 %+% fig5_data + facet_wrap(epa~type,labeller=epa_labeller)
+fig5 <- fig4 %+% fig5_data  + xlab("Identities")
 ggsave("paper/results_5.png",w=12,h=6,dpi=400)
 
 d <- fig5_data[epa ==0,list(n=sum(n_samples),t=sum(n_samples > 0)),by='entity']
